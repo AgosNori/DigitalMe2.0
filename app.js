755e7d511ec8f29506bbc03944464863627e7b7d
agos <agosnoriega2018@gmail.com>
2025-02-15 20:29:15 -0300

    Subiendo la pagina principal actualzada

---
+++ app.js
@@ -0,0 +1,33 @@
+const loaderElement = document.querySelector(".contenedorLoader");
+const header = document.querySelector('.header');
+let acc = document.getElementsByClassName("accordion");
+const btnMenu = document.querySelector(".menuResp");
+const navHeader = document.querySelector(".navBarHeader");
+const iconoheader = document.querySelector(".fa-bars");
+
+
+
+
+const linkImg = document.querySelector("#contenedorRoadmap");
+const lupa= document.querySelector(".fa-shake")
+const img=document.querySelector(".imgRoadmap");
+linkImg.onmouseover= () =>{
+    lupa.classList.remove("ocultar");
+    lupa.classList.add("centrar")
+   
+}
+linkImg.onmouseout = () =>{
+    lupa.classList.add("ocultar");
+    lupa.classList.remove("centrar")
+ 
+}
+
+// Evento de load para ocultar el loader
+window.addEventListener('load', function() {
+    if (loaderElement) {
+        loaderElement.classList.add("loader-hidden");
+        console.log("Loader ocultado");
+    } else {
+        console.error("No se encontró el elemento del loader");
+    }
+});
